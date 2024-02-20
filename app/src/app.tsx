fed13a3f91c3661a4eecf99223c67ffdb7f01e68
naiaragabriela <naigabi31@gmail.com>
2024-02-19 22:31:55 -0300

    feat: create a react app

---
+++ app/src/app.tsx
@@ -0,0 +1,19 @@
+
+export function App() {
+
+  return (
+   <div className="py-10 space-y-8">
+    <div>
+      header
+    </div>
+    <div>
+      abas
+    </div>
+    <div>
+      content
+    </div>
+   </div>
+  )
+}
+
+
